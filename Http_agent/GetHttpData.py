9e1a15715ed254863a129870b5eb426ca6147f70
Jun Wang <wangjun@navinfo.com>
2016-12-30 22:51:57 +0800

    提交http请求抓包操作

--- Http_agent/GetHttpData.py
+++ Http_agent/GetHttpData.py
@@ -41,13 +41,48 @@ try:
 except ImportError:
     # If you installed this package via pip, you just need to execute this
     from scapy.layers import http
-packets = scapy.rdpcap('E://GitHub//ideaPartnerX//Http_agent//soursePath//tcpdump.pcap')
-print packets
+import re
+packets = scapy.rdpcap('soursePath/tcpdump.pcap')
+#保存请求集
+HttpItems=[]
+for p in packets:
+    httpItem={"HostIP":None,"HttpCode":None,"ReqData":None,"AckData":None,"ReqTime":None,"AckTime":None}
+    if 'Raw' in p and p['Raw'] and (re.findall(r'GET .*? HTTP/1.1',p['Raw'].load) or re.findall(r'POST .*? HTTP/1.1',p['Raw'].load)):
+        httpItem["HostIP"] = p['IP'].dst
+        httpItem["HttpCode"] = p['TCP'].ack
+        httpItem["ReqData"] = p['Raw'].load
+        # httpItem["ReqTime"] = p['Raw'].load
+        HttpItems.append(httpItem)
+#补登返回集
+count=0
+for HttpItem in HttpItems:
+    for p in packets:
+        if 'Raw' in p and p['Raw'] and p['Raw'].load and HttpItem["HttpCode"] == p['TCP'].seq:
+            HttpItems[count]["AckData"]=p['Raw'].load
+    count+=1
+for http_Item in HttpItems:
+    print '=' * 78+'='
+    print http_Item["HostIP"]
+    print '=' * 78+'*'
+    print http_Item["HttpCode"]
+    print '=' * 78+'*'
+    print http_Item["ReqData"]
+    print '=' * 78+'*'
+    print http_Item["AckData"]
+    print '=' * 78+'='
 for p in packets:
     print '=' * 78
-    if 'Raw' in  p:
-        print p.show()
-        print p['Raw']
+    print p.show()
+    # if 'Raw' in  p:
+    #     print p.show()
+    #     print '*'*78
+    #     print p['IP'].src
+    #     print p['TCP'].seq
+    #     print p['TCP'].ack
+    #     print '*'*78
+    #     print p['Raw'].load
+
+
 # #print p.show()
 #     for f in p.payload.fields_desc:
 #         if f.name == 'src' or f.name == 'dst':
