5dc65d018e08bdc2d3e9ffe864725e6250fc809f
Jo531 <506293327@qq.com>
2017-01-13 18:24:31 +0800

    修改冒烟执行方式，同时支持要素和全部
    修改冒烟执行方式，同时支持要素和全部

--- runSmoke.py
+++ runSmoke.py
@@ -10,41 +10,19 @@ import sys
 
 if __name__ == '__main__':
     Logger = logger(logfilename)
-
     print (u"="*60)
-    print u"【指令格式】：大区库dbid;执行人userid;指令类型;参数 ...\n" \
-          u"\n" \
-          u"【参数说明】\n" \
-          u"1、根据fiddler抓包执行数据初始化：\n" \
-          u"指令类型: 1\n" \
-          u"参数: saz文件所在绝对路径\n" \
-          u"【示例】257;3680;1;D:/fiddlerdata\n" \
-          u"\n" \
-          u"2、根据履历构建的测试数据集id执行测试：\n" \
-          u"指令类型: 2\n" \
-          u"参数: saz文件名称的列表，英文逗号','分隔\n" \
-          u"【示例】257;3680;2;saz_filename1,saz_filename2\n" \
-          u"\n" \
-          u"【使用说明】\n" \
-          u"1、执行时先执行指令类型=1再执行指令类型=2。\n" \
-          u"2、执行时，仅需根据需要修改对应的大区库dbid、执行人userID、指令类型、参数即可。\n" \
-          u"3、过程中如有路径，请避免路径带空格。"
+    print u"请选择执行内容：\n" \
+          u"1、根据fiddler抓包执行数据初始化\n" \
+          u"2、根据测试要素执行测试并验证\n"
     print (u"="*60)
-    print u"请输入指令："
-    cmd = raw_input()
-    pars = cmd.split(';')
-    dbid = pars[0]
-    userid = pars[1]
-    cmdtype = pars[2]
+    cmdtype = raw_input()
     if cmdtype == '1':
-        zipdir = pars[3]
-        # unzipdir = pars[4]
+        zipdir = ConfFilename.getElementsByTagName('zipdir')[0].firstChild.data
         unzipdir = os.path.dirname(os.path.realpath(sys.path[0]))+os.sep+'unzipdir'
         if not os.path.exists(unzipdir):
             os.mkdir(unzipdir)
-        IBD = IniBaseData(LogTestDBConf, Logger, zipdir, unzipdir)
-        IBD.run()
+        IniBaseData(LogTestDBConf, Logger, zipdir, unzipdir).run()
     elif cmdtype == '2':
-        SR = SmokeRunner(dbid, userid, LogTestDBConf, Logger)
-        c_idlist = pars[3].decode('gbk').encode('utf-8').split(',')
-        SR.run(c_idlist)
+        dbid = ConfFilename.getElementsByTagName('dbid')[0].firstChild.data
+        userid = ConfFilename.getElementsByTagName('userid')[0].firstChild.data
+        SmokeRunner(dbid, userid, LogTestDBConf, Logger).run()
